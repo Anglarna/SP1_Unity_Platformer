faf98ceafa75dcd7946dcf7fbd7f1c7eabcc3dd7
EvanAnglar <ivan.wendelbommarco@gmail.com>
2026-09-23 18:00:00 +0200

    particles + player health
    added particles + set player health to 1

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,22 +6,17 @@ public class PlayerHealth : MonoBehaviour
 
     [SerializeField] private int startingHealth = 5;
     [SerializeField] private Transform spawnPosition;
-    [SerializeField] private Slider healthSlider;
-    [SerializeField] private Image fillImage;
     [SerializeField] private Color normalHealthColor, criticalHealthColor;
     private int currentHealth;
 
     void Start()
     {
         currentHealth = startingHealth;
-        healthSlider.value = currentHealth;
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth-= damage;
-
-        UpdateHealthbar();
 
         if (currentHealth <= 0)
         {
@@ -32,24 +27,9 @@ public class PlayerHealth : MonoBehaviour
     private void Respawn()
     {
         currentHealth = startingHealth;
-        UpdateHealthbar();
 
         transform.position = spawnPosition.position;
         GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
-    }
-
-    private void UpdateHealthbar()
-    {
-        healthSlider.value = currentHealth;
-
-        if (currentHealth <= 2)
-        {
-            fillImage.color = criticalHealthColor;
-        }
-        else
-        {
-            fillImage.color = normalHealthColor;
-        }
     }
 
     public bool RestoreHealth(int healthToRestore)
@@ -59,7 +39,6 @@ public class PlayerHealth : MonoBehaviour
             return false;
         }
         currentHealth += healthToRestore;
-        UpdateHealthbar();
 
         if(currentHealth > startingHealth)
         {
